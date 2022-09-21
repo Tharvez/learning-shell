a6731f6b9c6addeb4df863bc6dce809858440c9a
Abdul Hakeem Habeeb Rahman <abdul@ilink-systems.com>
2022-09-21 13:17:42 +0530

    Getting input from CLI through read command

--- 04-specialVariables.sh
+++ 04-specialVariables.sh
@@ -1,8 +1,7 @@
-
-echo $0 # script name
-echo $1 # first argument
-echo $2 # 2nd argument
-echo $* # all argument
-echo $@ # all argument
-echo $# # no of argument
-echo Status = $?
+LOG_FILE = /etc/tmp
+echo $1 &>>$LOG_FILE # first argument
+echo $2 &>>$LOG_FILE # 2nd argument
+echo $* &>>$LOG_FILE # all argument
+echo $@ &>>$LOG_FILE # all argument
+echo $# &>>$LOG_FILE # no of argument
+echo Status = $? &>>$LOG_FILE
