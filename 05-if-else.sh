283076cd70c613ad5015aaf6dd6457f69f40108d
Abdul Hakeem Habeeb Rahman <abdul@ilink-systems.com>
2022-09-21 17:41:04 +0530

    Getting input from CLI through read command

--- 05-if-else.sh
+++ 05-if-else.sh
@@ -1,9 +1,9 @@
-a=1
+read -p 'Enter number : ' a
 
 if [ $a -gt 10 ]
 then
-  echo $a is greater than 10 $a
+  echo $a is greater than 10
 else
-  echo $a is less than 10 $a
+  echo $a is less than 10
 fi
 
