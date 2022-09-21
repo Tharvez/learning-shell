600d44f41dcd927a001507f3564567df8524cbc3
Abdul Hakeem Habeeb Rahman <abdul@ilink-systems.com>
2022-09-21 17:42:51 +0530

    Getting input from CLI through read command

--- 05-if-else.sh
+++ 05-if-else.sh
@@ -7,3 +7,14 @@ else
   echo $a is less than 10
 fi
 
+x="abc"
+if [ $x == "abc" ]; then
+  echo Yes both are equal
+fi
+
+file=/tmp/new
+if [ -f $file ]; then
+  echo File Exists
+else
+  echo File does not exist
+fi
